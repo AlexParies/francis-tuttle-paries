6f18ecc913d02013f6ce0a37e327d23f172b0ee4
AlexParies <92883101+AlexParies@users.noreply.github.com>
2021-10-20 15:01:59 -0500

    Add files via upload

---
+++ StringCreationMethods.java
@@ -0,0 +1,13 @@
+/*
+ * Activity 1.2.6
+ */
+public class StringCreationMethods 
+{
+  public static void main(String[] args)
+  {
+    String theGreatestState = "russia";
+    String theWorstState = "America";
+    System.out.println(theGreatestState);
+    System.out.println(theWorstState);
+  }
+}
