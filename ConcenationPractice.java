cd5e8cb8631b3e4bf9c0622b00a03d0d3387d877
AlexParies <92883101+AlexParies@users.noreply.github.com>
2021-10-21 12:47:35 -0500

    Add files via upload

---
+++ ConcenationPractice.java
@@ -0,0 +1,18 @@
+/*
+ * Activity 1.2.6
+ */
+public class ConcenationPractice
+{
+  public static void main(String[] args) 
+  {
+    String stateTreat = "Whoopie Pie";
+    String stateBerry = "Wild Berry";
+    
+    /* Use the + operator on stateTreat to append " Pie" */
+    
+    /* Use the += operator on stateBerry to append "Blueberry" */
+    
+    System.out.println(stateTreat);
+    System.out.println(stateBerry);
+  }
+}
